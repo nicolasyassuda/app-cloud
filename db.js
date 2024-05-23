98f85209e1a1fbb8d20e3d0e4e9c012ee7c52393
nicolasyassuda <92542916+nicolasyassuda@users.noreply.github.com>
2024-05-23 11:46:21 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 const AWS = require('aws-sdk');
 AWS.config.update({
-  accessKeyId: process.env.DB_ACCESS_KEY_ID,
-  secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
   region: 'us-east-2',
 });
 const db = new AWS.DynamoDB.DocumentClient({ apiVersion: 'latest' });
